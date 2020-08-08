4e4be60fe2495677b184f8c6187cc7058649304b
Vanessa Lane <vlane@23andme.com>
2020-08-07 18:06:34 -1000

    add route for delete, fix bug with the get route

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,23 +1,34 @@
-
+const uniqid = require('uniqid');
 const router = require('express').Router();
-const notes = require('../../db/db.json');
-const { saveNewNote, validateNote } = require("../../lib/notes");
+const { loadNotesFromFile, deleteNoteById, saveNewNote, validateNote } = require("../../lib/notes");
 
 router.get('/notes', (req, res) => {
+    const notes = loadNotesFromFile();
     res.json(notes);
 })
 
 router.post('/notes', (req, res) => {
     // set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
+    req.body.id = uniqid();
 
     // add note to the db.json file
     if (!validateNote(req.body)) {
         res.status(400).send('The note does not have a title and body.');
     } else {
+        const notes = loadNotesFromFile();
         const note = saveNewNote(req.body, notes);
         res.json(note);
     }
 });
+
+router.delete('/notes/:id', (req, res) => {
+    const notes = loadNotesFromFile();
+    if (req.params.id) {
+        deleteNoteById(req.params.id, notes);
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
+    }
+})
 
 module.exports = router;
