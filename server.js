23decac836c613a721f7d7eb5b22632626ef31fc
Vanessa Lane <vlane@23andme.com>
2020-08-07 14:11:27 -1000

    Adds HTML route for notes page

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+// serve static files
+app.use(express.static('public'));
+// use routes
+app.use('/', htmlRoutes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
